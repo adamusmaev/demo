f4b6c97c1c28af4abb46f5874c9e5585b288e28b
adamusmaev <adam.usmaev2000@mail.ru>
2021-04-15 13:41:47 +0300

    Create Spring MVC Application

---
+++ src/main/java/com/example/demo/controllers/Main.java
@@ -0,0 +1,21 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@Controller
+public class Main {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
+
